cbff96b001826f1e7399b29b92d97bda6d5c39fd
Sergio C <sergiocalvosos@gmail.com>
2020-04-08 17:48:01 +0200

    Inicio plantilla para proyecto TODO

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { saludar } from './js/componentes.js';
+import { Todo } from './classes/todo.class.js';
+
 import './styles.css';
 
-
-const nombre = 'Fernando';
-
-saludar( nombre );
+const tarea = new Todo('aprender JS');
+console.log(tarea);
