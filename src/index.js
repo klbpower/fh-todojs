0e80279cb304f3554fce590de373a1c753e29ee5
Sergio C <sergiocalvosos@gmail.com>
2020-04-08 18:04:11 +0200

    Clases Todo TodoList y Ordenando con un import unico

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { saludar } from './js/componentes.js';
-import { Todo } from './classes/todo.class.js';
-
+//import { Todo } from './classes/todo.class.js';
+//import { TodoList } from './classes/todo-list.class.js';
 import './styles.css';
+import {Todo, TodoList} from './classes';
+/* Por defecto al importar classes coge index.js , espectacular, no hay que indicarlo*/ 
 
+const todoList = new TodoList();
 const tarea = new Todo('aprender JS');
 console.log(tarea);
