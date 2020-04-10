192fc7fd489f67b0afb217c62a174df8c055d194
Sergio C <sergiocalvosos@gmail.com>
2020-04-11 00:53:49 +0200

    LocalStorage Todos

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,23 +1,46 @@
 export class TodoList{
     constructor(){
-        this.todos = [];
+        this.cargarLocalStorage();
+        //this.todos = [];
     }
     nuevoTodo( todo ){
         this.todos.push( todo );
+        this.guardarLocalStorage();
     }
     eliminarTodo( id ){
         this.todos = this.todos.filter( todo => todo.id != id );
+        this.guardarLocalStorage();
     }
     marcarCompletado( id ){
         for( const todo of this.todos ){
             if( todo.id == id ){
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
     }
     eliminarCompletados( ){
         this.todos = this.todos.filter( todo => !todo.completado );
+        this.guardarLocalStorage();
 
+    }
+
+    guardarLocalStorage( ){
+        localStorage.setItem('todo', JSON.stringify(this.todos) );
+
+    }
+    cargarLocalStorage(){
+        if( localStorage.getItem('todo') ) {
+            this.todos = JSON.parse( localStorage.getItem('todo') );
+            /*console.log("cargando localstorage", this.todos);
+            console.log(typeof this.todos );*/
+        }else{
+            this.todos = [];
+        }
+        //opcion ternaria
+        /*this.todos = ( localStorage.getItem('todo') ) 
+                        ? JSON.parse( localStorage.getItem('todo') )
+                        : [];  */
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
+import './styles.css';
+import {Todo, TodoList} from './classes';
 import { saludar, crearTodoHtml } from './js/componentes.js';
 //import { Todo } from './classes/todo.class.js';
 //import { TodoList } from './classes/todo-list.class.js';
-import './styles.css';
-import {Todo, TodoList} from './classes';
+
+
 /* Por defecto al importar classes coge index.js , espectacular, no hay que indicarlo*/ 
 
 export const todoList = new TodoList();
 
-const tarea = new Todo('aprender JS');
+console.log( todoList.todos );
+todoList.todos.forEach( todo => crearTodoHtml(todo) );
+
+/*const tarea = new Todo('aprender JS');
 
 todoList.nuevoTodo( tarea );
 
 console.log( todoList );
 
-crearTodoHtml( tarea );
+crearTodoHtml( tarea );*/
+
+/*localStorage.setItem('mikey','abc123');
+setTimeout( ()=>{
+    localStorage.removeItem('mikey');
+}, 1500 );*/
